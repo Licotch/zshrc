97f50db34a2da7cc854f8f4ce99109818ba2271b
Licotch <licotch@licot.ch>
2018-09-09 18:02:45 +0900

    add command not found handler

--- .zsh.d/prompt/prompt.zsh
+++ .zsh.d/prompt/prompt.zsh
@@ -2,6 +2,10 @@
 
 SPROMPT="%F{magenta}(๑-ิ ‸-ิ๑) < %B%r%b だと思うのじゃが… %f"
 
+function command_not_found_handler {
+    echo -e "$fg[magenta](๑-ิ ‸-ิ๑) < \e[1m$1\e[0m $fg[magenta]というコマンドはないみたいじゃの"
+}
+
 if (tty|fgrep -q 'tty'); then
     source $ZDOTDIR/prompt/tty.zsh
 else
